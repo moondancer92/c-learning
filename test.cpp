c0d7e112a1068465eb727166209a3aab7ceff36f
Sung Seunghyun <70056397+moondancer92@users.noreply.github.com>
2024-03-31 17:43:24 +0900

    StringClass_quiz
    StringClass_quiz

--- test.cpp
+++ test.cpp
@@ -3,46 +3,122 @@
 */
 
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-const int kMaxStr = 100; // 전역 상수 소개
+// public, private 접근 권한 안내
 
-// 문자열을 매개변수로 넣기
-// 여기서는 모든 문자열 배열의 길이가 동일하다고 가정
-bool IsEqual(const char str1[], const char str2[])
+class MyString
 {
-    // 크기 출력 확인 (배열 크기가 아님 주의!) - 문자열의 길이를 별도로 저장해야 합니다!
-    //cout << sizeof(str1) << " " << sizeof(str2) << " " << endl;
-   //exit(-1);
-    for(int i = 0; i < kMaxStr; i++){
-        if(str1[i] != str2[i]){
-            return false;
-        }
-        // 문자열 종료 조건
-        if(str1[i] == '\0')
-            return true;
+public:
+    MyString()
+    {
+        // 호출 시점 확인
+        cout << "MyString()" << endl;
+
+        size_ = 1;
+        str_ = new char[size_];
     }
-    // 힌트: ==, != 같지 않다 비교 연산자
-       
-    // 디버깅 힌트: 문자를 정수로 바꿔서 출력해보기
-    return true;
-}
+
+    MyString(const char *init_str) // init_str이 유효한 메모리라고 가정
+    {
+        cout << "MyString(const char *init_str)" << endl;
+
+        // 1. 글자 수 먼저 확인
+        size_ = 0;
+        while (init_str[size_] != '\0')
+        {
+            size_++; // ++ 연산자 안내
+        }
+
+        // 2. 글자 수가 0이 아니면 메모리 할당
+        if (size_ > 0)
+        {
+            str_ = new char[size_];
+        }
+
+        // 3. 복사
+        for (int i = 0; i < size_; i++)
+        {
+            str_[i] = init_str[i];
+        }
+        // memcpy() 사용 가능
+    }
+
+    ~MyString()
+    {
+        // 호출 시점 확인
+        cout << "Destructor" << endl;
+
+        size_ = 0;
+        if (str_)
+            delete[] str_;
+    }
+
+    void Resize(int new_size)
+    {
+        char *new_str = new char[new_size];
+
+        // memcpy() 사용 가능
+        for (int i = 0; i < (new_size < size_ ? new_size : size_); i++)
+        {
+            new_str[i] = str_[i];
+        }
+
+        delete[] str_;
+        str_ = new_str;
+        size_ = new_size;
+
+        // new_str 지우면 안되요!
+    }
+
+    void Print()
+    {
+        for (int i = 0; i < size_; i++)
+        {
+            cout << str_[i];
+        }
+        cout << endl;
+    }
+
+    void Append(MyString *app_str) // 같은 타입을 매개변수로 사용 가능
+    {
+        int old_size = size_;
+        int new_size = old_size + app_str->size_;
+        
+        cout <<"old_size: " <<size_ << "\n";
+        cout <<"new_size: " <<new_size << "\n";
+        // 다른 멤버 함수 호출 가능
+        // Resize(...);
+        Resize(new_size);
+        int i_app_str = 0;
+        // 중요한 개념
+        for (int i = old_size; i < size_; i++)
+        {
+            // TODO: 복사
+            
+            str_[i] = app_str -> str_[i_app_str];
+            i_app_str++;
+        }
+    }
+
+private:
+    int size_ = 0;        // m_size
+    char *str_ = nullptr; // m_str (여기서는 구글 스타일)
+};
 
 int main()
 {
-    // 영어 사용이 디버깅에 유리합니다.
-    const char str1[kMaxStr] = "stop";
-   
-    while (1)
-    {
-        char str2[kMaxStr];
-        cin >> str2;
-        
-        if(IsEqual(str1,str2))
-        {
-            break;
-        }
-       }
+    // 클래스 기본 문법 안내
+
+    MyString str1("ABCDE"); // 생성자 이용
+    MyString str2("123");
+
+    str1.Append(&str2); // 주소 넣어줌
+
+    str1.Print();
+
     return 0;
 }
+
